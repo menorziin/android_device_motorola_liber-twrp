bfa82149408c517ea548097f48d5f7e38bea2c65
menorziin <williamsilvapppl@gmail.com>
2023-01-30 04:25:20 -0300

    liber: Adapt to OrangeFox
    Change-Id: Icb6a801fbcfb8ea2bdeb50e14dde62eb0504adb0

---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+export OF_AB_DEVICE=1
+export LC_ALL="C"
+export ALLOW_MISSING_DEPENDENCIES=true
+export FOX_VERSION="R12.1"
+export OF_MAINTAINER="William"
+export OF_SCREEN_H=2340
+export OF_STATUS_H=100
+export OF_STATUS_INDENT_LEFT=39
+export OF_STATUS_INDENT_RIGHT=39
+export OF_CLOCK_POS="1"
